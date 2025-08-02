faa09ea1ae3c05266bd709c45e871c75c43a7caf
rahul-boga-19 <rahulboga62@gmail.com>
2025-08-02 15:56:17 +0530

    first commit

---
+++ dashboard/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb://localhost:27017/") 
+db = client['intern_portal']
+collection = db['interns']
+
+def index(request):
+    return render(request, 'dashboard/index.html')
+
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
+
+def leaderboard(request):
+    return render(request, 'dashboard/leaderboard.html')
+
+def api_intern(request):
+
+    intern = collection.find_one({})
+    if intern:
+        data = {
+            'name': intern.get('name'),
+            'referral_code': intern.get('referral_code'),
+            'donations_raised': intern.get('donations_raised'),
+        }
+        print(data)
+    else:
+        data = {
+            'name': 'No intern found',
+            'referral_code': 'N/A',
+            'donations_raised': 0
+        }
+    return JsonResponse(data)
